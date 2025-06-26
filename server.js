35feabfb7deeff1463c94f14cc2582dc8516cefd
sweetjojo69 <138777891+sweetjojo69@users.noreply.github.com>
2025-06-26 18:15:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 let players = [];
 let matches = [];
